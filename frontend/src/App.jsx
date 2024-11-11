57b46df96f8778944e4110bbdb30ed8577b9fcf1
Himanshu-coder007 <himanshunagose54@gmail.com>
2024-11-11 20:51:07 +0530

    front end added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navbar from './components/shared/Navbar'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <Navbar/>
+    </>
+  )
+}
+
+export default App
