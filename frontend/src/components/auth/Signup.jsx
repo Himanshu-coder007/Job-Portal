95202804fda1186325b9a1fc3f052b8310523570
Himanshu-coder007 <himanshunagose54@gmail.com>
2024-11-13 00:44:31 +0530

    signnup ui half done

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,13 +1,69 @@
-import React from 'react'
-import Navbar from '../shared/Navbar'
+import React from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const Signup = () => {
   return (
-    <div><Navbar/>
-    hello
+    <div>
+      <Navbar />
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          action=""
+          className="w-1/2 border border-gray-200 rounnded-md p-4 my-10"
+        >
+          <h1 className="font-bold text-xl mb-5">Sign Up</h1>
+          <div className="my-2">
+            <Label>Full Name</Label>
+            <Input type="text" placeholder="name" />
+          </div>
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input type="email" placeholder="email@gmail.com" />
+          </div>
+          <div className="my-2">
+            <Label>Phone Number</Label>
+            <Input type="text" placeholder="phone number" />
+          </div>
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input type="password" placeholder="password" />
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recruiter"
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r2">Recruiter</Label>
+              </div>
+            </RadioGroup>
+            <div className="flex items-center gap-2">
+              <Label>Profile</Label>
+              <Input 
+              accept="image/*"
+              type="file"
+              className="cursor-pointer"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
