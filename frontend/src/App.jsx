aea7418f758a41d8b066adf1f1b32d751ec2df97
Himanshu-coder007 <himanshunagose54@gmail.com>
2024-12-31 00:56:24 +0530

    started admin side ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import Jobs from "./components/Jobs";
 import Browse from "./components/Browse";
 import Profile from "./components/Profile";
 import JobDescription from "./components/JobDescription";
+import Companies from './components/admin/Companies'
+
 
 const appRouter = createBrowserRouter([
   {
@@ -37,6 +39,12 @@ const appRouter = createBrowserRouter([
     path: "/profile",
     element: <Profile />,
   },
+  
+  //client-side routing for admin
+  {
+    path:"/admin/companies",
+    element: <Companies/>,
+  }
 ]);
 function App() {
   return (
