97175a94d30dda30660e0f0e5c2e96d06f562579
Himanshu-coder007 <himanshunagose54@gmail.com>
2025-01-03 22:55:19 +0530

    Building Admin Job  Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import JobDescription from "./components/JobDescription";
 import Companies from './components/admin/Companies'
 import CompanyCreate from "./components/admin/CompanyCreate";
 import CompanySetup from "./components/admin/CompanySetup";
+import AdminJobs from "./components/admin/AdminJobs";
 
 
 
@@ -55,6 +56,10 @@ const appRouter = createBrowserRouter([
   {
     path:"/admin/companies/:id",
     element: <CompanySetup/>,
+  },
+  {
+    path:"/admin/jobs",
+    element: <AdminJobs/>,
   },
 ]);
 function App() {
