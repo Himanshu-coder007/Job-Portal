a04a26f93cfaf5f333a12820d67c358cfbbb52aa
Himanshu-coder007 <himanshunagose54@gmail.com>
2025-01-05 23:50:28 +0530

    creating post job

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -6,7 +6,15 @@ import { Input } from '../ui/input'
 const PostJob = () => {
   return (
     <div>
-
+ <Navbar/>
+ <div className='flex items-center justify-center w-screen my-5'>
+ <div>Title</div>
+ <Input
+ type='text'
+ name="title"
+ className="focus-visible:ring-offset-0 focus-visible:ring-0 my-1"
+ />
+ </div>
     </div>
   )
 }
