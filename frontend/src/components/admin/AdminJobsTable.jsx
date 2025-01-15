09c827931a64bdc26aa881e9c6912e08b317c4ce
Himanshu-coder007 <himanshunagose54@gmail.com>
2025-01-15 23:50:39 +0530

    Impleamented Applicant Page

--- frontend/src/components/admin/AdminJobsTable.jsx
+++ frontend/src/components/admin/AdminJobsTable.jsx
@@ -49,7 +49,8 @@ const AdminJobsTable = () => {
                                                 <Edit2 className='w-4' />
                                                 <span>Edit</span>
                                             </div>
-                                            <div onClick={()=> navigate(`/admin/jobs/${job._id}/applicants`)} className='flex items-center w-fit gap-2 cursor-pointer mt-2'>
+
+                                         <div onClick={()=> navigate(`/admin/jobs/${job._id}/applicants`)} className='flex items-center w-fit gap-2 cursor-pointer mt-2'>
                                                 <Eye className='w-4'/>
                                                 <span>Applicants</span>
                                             </div>
